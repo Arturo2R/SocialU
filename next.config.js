ec2086c40b9959e78c22d80275192389ad2cf91c
Arturo Rebolledo <elrayoarturopro@outlook.com>
2022-05-08 22:56:48 -0500

    added referrer policy header

--- next.config.js
+++ next.config.js
@@ -10,4 +10,16 @@ module.exports = withBundleAnalyzer({
   images: {
     domains: ['source.unsplash.com', 'example2.com'],
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: [{
+          key: 'Referrer-Policy',
+          value: 'origin-when-cross-origin'
+        }],
+      },
+    ]
+  },
 });
