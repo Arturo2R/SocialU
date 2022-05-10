ec14c4dfa2724656d9a83f5ec12b0eaad8dc38b2
Arturo Rebolledo <elrayoarturopro@outlook.com>
2022-05-10 16:54:44 -0500

    cosita

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -6,12 +6,12 @@ import {
   Text,
   Title,
   Center,
-} from '@mantine/core';
-import { useState } from 'react';
-import { useFirestore } from '../../hooks/useFirestore';
+} from "@mantine/core";
+import { useState } from "react";
+import { useFirestore } from "../../hooks/useFirestore";
 
-import { Post } from '../Post/Post';
-import useStyles from './Feed.styles';
+import { Post } from "../Post/Post";
+import useStyles from "./Feed.styles";
 
 export function Feed() {
   const { classes } = useStyles();
@@ -22,12 +22,12 @@ export function Feed() {
   const { data, error: dataError, loading } = useFirestore();
   console.log(data);
   if (loading) {
-return (
-      <Center className="my-auto h-full">
+    return (
+      <Center className="h-full my-auto">
         <Loader color="orange" size="lg" variant="bars" />
       </Center>
     );
-}
+  }
   if (error) return <Text>{error}</Text>;
 
   return (
@@ -47,18 +47,22 @@ return (
         {/* // !TODO: Use React Suspense */}
 
         <Space h="md" />
-        <Stack spacing="lg">
+        <Stack spacing="lg" className="max-w-sm mx-auto">
           {data &&
-            data.map((post) => (
+            data.map((post, index) => (
               <Post
                 description={post.message}
-                author={{ name: post.authorName, id: post.userUID }}
+                author={
+                  post.anonimo
+                    ? "anonimo"
+                    : { name: post.authorName, id: post.userUID, image: "fsl" }
+                }
                 title={post.title}
                 image={post.image}
                 // asistants={post.asistants} // Añadir asistentes
                 postId={post.id}
                 event={post.isEvent}
-                key={post.id}
+                key={index}
               />
             ))}
 
@@ -66,9 +70,9 @@ return (
             description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque laudantium officia, ipsum enim tempore molestias architecto itaque? Vel aut nam at voluptatem, sunt, quo laudantium similique dolore, asperiores laborum nostrum."
             author={{
               image:
-                'https://cr00.epimg.net/radio/imagenes/2021/01/02/tendencias/1609606240_435257_1609606414_noticia_normal.jpg',
-              name: 'María Juana',
-              id: 'kjsdflk',
+                "https://cr00.epimg.net/radio/imagenes/2021/01/02/tendencias/1609606240_435257_1609606414_noticia_normal.jpg",
+              name: "María Juana",
+              id: "kjsdflk",
             }}
             title="Hola Que Hace"
             // image="https://source.unsplash.com/random/180x90"
@@ -76,54 +80,54 @@ return (
             event
             asistants={[
               {
-                id: 'jlfksd',
-                name: 'El Kangas',
-                avatar: '/perfil.jpg',
+                id: "jlfksd",
+                name: "El Kangas",
+                avatar: "/perfil.jpg",
               },
               {
-                id: 'jlfksd',
-                name: 'El Carecu',
-                avatar: '/perfil.jpg',
+                id: "jlfksd",
+                name: "El Carecu",
+                avatar: "/perfil.jpg",
               },
               {
-                id: 'jlfksd',
-                name: 'El Brayan',
-                avatar: '/perfil.jpg',
+                id: "jlfksd",
+                name: "El Brayan",
+                avatar: "/perfil.jpg",
               },
               {
-                id: 'jlfksd',
-                name: 'El Manotas',
-                avatar: '/perfil.jpg',
+                id: "jlfksd",
+                name: "El Manotas",
+                avatar: "/perfil.jpg",
               },
               {
-                id: 'jlfksd',
-                name: 'El Gato Volador',
-                avatar: '/perfil.jpg',
+                id: "jlfksd",
+                name: "El Gato Volador",
+                avatar: "/perfil.jpg",
               },
               {
-                id: 'jlfksd',
-                name: 'El Kangas',
-                avatar: '/perfil.jpg',
+                id: "jlfksd",
+                name: "El Kangas",
+                avatar: "/perfil.jpg",
               },
               {
-                id: 'jlfksd',
-                name: 'El Carecu',
-                avatar: '/perfil.jpg',
+                id: "jlfksd",
+                name: "El Carecu",
+                avatar: "/perfil.jpg",
               },
               {
-                id: 'jlfksd',
-                name: 'El Brayan',
-                avatar: '/perfil.jpg',
+                id: "jlfksd",
+                name: "El Brayan",
+                avatar: "/perfil.jpg",
               },
               {
-                id: 'jlfksd',
-                name: 'El Manotas',
-                avatar: '/girl.jpg',
+                id: "jlfksd",
+                name: "El Manotas",
+                avatar: "/girl.jpg",
               },
               {
-                id: 'jlfksd',
-                name: 'El Gato Volador',
-                avatar: '/perfil.jpg',
+                id: "jlfksd",
+                name: "El Gato Volador",
+                avatar: "/perfil.jpg",
               },
             ]}
           />
