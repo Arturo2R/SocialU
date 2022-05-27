db8291a90e4b6815bc170c10fdf1457c6906b293
Arturo .R <arturoreb2r@outlook.com>
2022-05-27 16:14:35 +0000

    Making It PWA

--- app/next.config.js
+++ app/next.config.js
@@ -2,10 +2,15 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const withPWA = require('next-pwa')
+
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  pwa: {
+    dest: 'public'
   },
   images: {
     domains: ['source.unsplash.com', 'example2.com'],
---
+++ app/src/pages/_offline.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+type Props = {};
+
+const _offline = (props: Props) => {
+  return <Layout>Estamos Offline</Layout>;
+};
+
+export default _offline;
