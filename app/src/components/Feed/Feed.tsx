b47dbd5c8b5f5f06eedff12ec1ea88076354b8c3
Arturo .R <arturoreb2r@outlook.com>
2022-05-28 18:32:03 +0000

    withServerSide Props in the HomePage

--- app/src/components/Feed/Feed.tsx
+++ app/src/components/Feed/Feed.tsx
@@ -13,17 +13,24 @@ import { Suspense } from "react";
 import { Post } from "../Post/Post";
 import useStyles from "./Feed.styles";
 
-export function Feed() {
+export function Feed({ data }: anything) {
   const { classes } = useStyles();
 
   // error state
   const [error, setError] = useState<Error | null>(null);
   const [isLoading, setIsLoading] = useState<"loading" | "loaded">("loading");
-  const { data, error: dataError, postsLoading, fetchData } = useFirestore();
+  const {
+    data: liveData,
+    error: dataError,
+    postsLoading,
+    fetchData,
+  } = useFirestore();
   // console.log(data);
   useEffect(() => {
-    // setIsLoading("loading");
-    fetchData();
+    setIsLoading("loading");
+    fetchData().then(() => setIsLoading("loaded"));
+    // return () => {
+    // };
   }, []);
 
   // if (postsLoading === "loading") {
@@ -38,53 +45,39 @@ export function Feed() {
   // console.log(postsLoading);
   return (
     <>
-      {postsLoading === "loading" && (
+      {/* {isLoading === "loading" && (
         <Center className="my-auto h-full">
           <Loader color="orange" size="lg" variant="bars" />
         </Center>
-      )}
-      {postsLoading === "loaded" && data && (
-        <Container className="p-0 lg:px-12">
-          {/* <Title className={classes.title} align="center">
-          Bienvenido A
-          <Text
-            inherit
-            className="ml-1 text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-600"
-            component="span"
-          >
-            SocialU
-          </Text>
-        </Title> */}
-
-          {/* // !TODO: Use React Suspense */}
-
-          <Space h="md" />
-          <Stack spacing="lg" className="mx-auto max-w-sm">
-            {data &&
-              data.map((post, index) => (
-                <Post
-                  description={post.message}
-                  author={
-                    post.anonimo
-                      ? "anonimo"
-                      : {
-                          name: post.authorName,
-                          id: post.userName ? post.userName : post.id,
-                          ...(post.authorImage && { image: post.authorImage }),
-                        }
-                  }
-                  title={post.title}
-                  image={post.image}
-                  // asistants={post.asistants} // Añadir asistentes
-                  postId={post.id}
-                  event={post.isEvent}
-                  key={index}
-                  asistants={post?.suscriptions}
-                />
-              ))}
-          </Stack>
-        </Container>
-      )}
+      )} */}
+      {/* {isLoading === "loaded" && ( */}
+      <Container className="p-0 lg:px-12">
+        <Space h="md" />
+        <Stack spacing="lg" className="mx-auto max-w-sm">
+          {(liveData ?? data).map((post: any, index: number) => (
+            <Post
+              description={post.message}
+              author={
+                post.anonimo
+                  ? "anonimo"
+                  : {
+                      name: post.authorName,
+                      id: post.userName ? post.userName : post.id,
+                      ...(post.authorImage && { image: post.authorImage }),
+                    }
+              }
+              title={post.title}
+              image={post.image}
+              // asistants={post.asistants} // Añadir asistentes
+              postId={post.id}
+              event={post.isEvent}
+              key={index}
+              asistants={post?.suscriptions}
+            />
+          ))}
+        </Stack>
+      </Container>
+      {/* )} */}
     </>
   );
 }
--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,11 +1,55 @@
+import {
+  collection,
+  getDoc,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+} from "@firebase/firestore";
 import { useMemo } from "react";
 import Feed from "../components/Feed";
 import Layout from "../components/Layout/Layout";
+import { db } from "../firebase";
 
-export default function HomePage() {
+export const getServerSideProps = async () => {
+  const q = query(
+    collection(db, "posts"),
+    orderBy("createdAt", "desc"),
+    limit(30)
+  );
+
+  const querySnapshot = await getDocs(q);
+
+  const data = querySnapshot.docs.map((doc: anything) => ({
+    id: doc.id,
+    ...doc.data(),
+    createdAt: doc?.data()?.createdAt?.toJSON(),
+    ...(doc.data().date !== "" && { date: doc?.data()?.date?.toJSON() }),
+    ...(typeof doc.data().time !== "string" && {
+      time: doc?.data()?.time?.toJSON(),
+    }),
+    // time: doc?.data()?.time?.toJSON(),
+    // date: doc.title,
+  }));
+
+  console.log(data);
+
+  return {
+    props: {
+      data,
+    }, // will be passed to the page component as props
+  };
+};
+
+interface HomeProps {
+  data: Object[];
+}
+
+export default function HomePage({ data }: HomeProps) {
+  console.log(data);
   return (
     <Layout>
-      <Feed />
+      <Feed data={data} />
     </Layout>
   );
 }
