16b1df865e7db26584971088a55f06a319b736a9
Arturo Rebolledo <arturo2r@outlook.com>
2024-01-12 19:12:04 -0500

    ntinf oun bg

--- app/src/components/notfound/NothingFoundBackgground.tsx
+++ app/src/components/notfound/NothingFoundBackgground.tsx
@@ -8,10 +8,9 @@ export function NothingFoundBackground() {
       <div className={classes.inner}>
         <Illustration className={classes.image} />
         <div className={classes.content}>
-          <Title className={classes.title}>Nothing to see here</Title>
+          <Title className={classes.title}>Aquí, no hay Nada</Title>
           <Text c="dimmed" size="lg" ta="center" className={classes.description}>
-            Page you are trying to open does not exist. You may have mistyped the address, or the
-            page has been moved to another URL. If you think this is an error contact support.
+            La página que intentas abrir no existe. Puede que hayas escrito mal la dirección, o la página ha sido movida a otra URL. Si piensas que esto es un error, contacta al soporte.
           </Text>
           <Group justify="center">
             <Button size="md">Take me back to home page</Button>
