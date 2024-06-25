7bc2c25d9963e6b3fe80cd065a3ebbd2334080e7
horbag <148023397+horbag@users.noreply.github.com>
2024-06-25 12:47:34 -0500

    Update config.js

--- app/src/config.js
+++ app/src/config.js
@@ -27,7 +27,7 @@ export default function () {
        //{ color: "#ED553B", name: "Clases", value: "clases" },
        { color: "#20639B", name: "Anuncios", value: "anuncios" },
        // Hola
-       { color: "#8000FF", name: "Parciales finales", value: "parciales_finales" },
+       { color: "#008000", name: "Matrícula 2024-3", value: "MatrículaUN" },
     ],
     appNames: ['Campus Gossip', "Chisme.app", "Desembuchalo", 'Chismes En La U', 'Student Secrets', "Campus Confessions", "UniConfesiones", "UniLeaks", "Campus Help", "Campus Connect",]
     
