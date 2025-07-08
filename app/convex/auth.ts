68c8fea253aa6a769fcd35161267388142031527
Arturo Rebolledo <arturoreb2r@outlook.com>
2025-07-08 16:12:00 -0500

    Update auth.ts

--- app/convex/auth.ts
+++ app/convex/auth.ts
@@ -6,8 +6,8 @@
 // export const { auth, signIn, signOut, store } = convexAuth({
 //   providers: [
 //     Microsoft({
-//       clientId: '2d826c10-9e32-43fc-8aff-45c19400feba',
-//       clientSecret: 'E-j8Q~mLaOOgXyIFCG4t_ORCd8q5CBn2ComqQdl-',
+//       clientId: '',
+//       clientSecret: '',
 //     }),
 //   ],
 // });
